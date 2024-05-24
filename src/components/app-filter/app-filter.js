ba332975d723731b2a46d4f6d587725e2aa52c0b
Олександр Кащенко <example@gmail.com>
2024-05-24 20:13:54 +0300

    Add filter

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,21 +1,24 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
+const AppFilter = ({filter, onFilterSelect }) => {
     return (
         <div className="btn-group">
             <button 
-            className="btn btl-light"
-            type="button">
+            className= {`btn ${filter === "all" ? "btl-light" : "btn-outline-light"}`}
+            type="button"
+            onClick={() => onFilterSelect ("all")}>
                 Все сотрудники
             </button>
             <button 
-            className="btn btl-outline-light"
-            type="button">
+            className={`btn ${filter === "rise" ? "btl-light" : "btn-outline-light"}`}
+            type="button"
+            onClick={() => onFilterSelect ("rise")}>
                 На повышение
             </button>
             <button 
-            className="btn btl-outline-light"
-            type="button">
+            className={`btn ${filter === "moreThan1000" ? "btl-light" : "btn-outline-light"}`}
+            type="button"
+            onClick={() => onFilterSelect ("moreThan1000")}>
                З/П больше 1000$
             </button>
         </div>
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,23 +11,17 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {name: "Alexander K.",salary: 5000,increase: false, rise: false, id: 1,},
-        { name: "Dima L.", salary: 1000, increase: false, rise: false, id: 2 },
+        { name: "Alexander K.", salary: 5000, increase: true, rise: false, id: 1 },
+        { name: "Dima L.", salary: 1000, increase: false, rise: true, id: 2 },
         { name: "Andrew S.", salary: 800, increase: false, rise: false, id: 3 },
       ],
       term: "",
+      filter: "all"
     };
   }
 
   deleteItem = (id) => {
     this.setState(({ data }) => {
-      // const index = data.findIndex((elem) => elem.id === id);
-
-      // const before = data.slice(0, index);
-      // const after = data.slice(index + 1);
-
-      // const newArr = [...before, ...after];
-
       return {
         data: data.filter((item) => item.id !== id),
       };
@@ -44,15 +38,9 @@ class App extends Component {
   onToggleIncrease = (id) => {
     this.setState(({ data }) => {
       const index = data.findIndex((elem) => elem.id === id);
-
       const old = data[index];
       const newItem = { ...old, increase: !old.increase };
-      const newArr = [
-        ...data.slice(0, index),
-        newItem,
-        ...data.slice(index + 1),
-      ];
-
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
       return {
         data: newArr,
       };
@@ -62,15 +50,9 @@ class App extends Component {
   onToggleRise = (id) => {
     this.setState(({ data }) => {
       const index = data.findIndex((elem) => elem.id === id);
-
       const old = data[index];
       const newItem = { ...old, rise: !old.rise };
-      const newArr = [
-        ...data.slice(0, index),
-        newItem,
-        ...data.slice(index + 1),
-      ];
-
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
       return {
         data: newArr,
       };
@@ -81,28 +63,41 @@ class App extends Component {
     if (term.length === 0) {
       return items;
     }
+    return items.filter((item) => item.name.indexOf(term) > -1);
+  };
 
-    return items.filter((item) => {
-      return item.name.indexOf(term) > -1;
-    });
+  filterEmp = (items, filter) => {
+    switch (filter) {
+      case "rise":
+        return items.filter(item => item.rise);
+      case "moreThan1000":
+        return items.filter(item => item.salary >= 1000);
+      default:
+        return items;
+    }
   };
 
   onUpdateSearch = (term) => {
     this.setState({ term });
   };
 
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
+
   render() {
-    const { data, term } = this.state;
-    const employees = this.state.data.length;
-    const increased = this.state.data.filter((item) => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
+    const { data, term, filter } = this.state;
+    const employees = data.length;
+    const increased = data.filter((item) => item.increase).length;
+    const visibleData = this.filterEmp(this.searchEmp(data, term), filter);
+
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
           <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-          <AppFilter />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
 
         <EmployersList
@@ -116,4 +111,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
